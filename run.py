d36df04b9650e4f97464d5dd7f610ca4c7e80d7a
vikdts <>
2023-09-13 16:16:11 +0000

    Create the board, plant the mines

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def create_board(grid=5, num_mines=5):
+    cell = 0
+    board = [[cell for c in range(grid)]for r in range(grid)]
+    mines_coord = []
+    plant_mines = 0
+    while plant_mines < num_mines:
+        loc = random.randint(0, grid**2 - 1)
+        row = loc // grid
+        col = loc % grid
+        if [row, col] in mines_coord:
+            continue
+        mines_coord.append([row, col])
+        plant_mines += 1
+    return board, mines_coord

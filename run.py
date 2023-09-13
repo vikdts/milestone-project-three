7bf0b475f32bb59dd385cc164fc5ae2a48ac0b1c
vikdts <>
2023-09-13 16:42:33 +0000

    Refactor play function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 
+
 def create_board(grid=5, num_mines=5):
     cell = 0
     board = [[cell for c in range(grid)]for r in range(grid)]
@@ -17,30 +18,27 @@ def create_board(grid=5, num_mines=5):
 
 
 def say_hello():
-    while True:
-        try:
-            user_input = input("Hello! This is minesweeper. \nFor Beginner press 'b', for Intermediate press 'i', for Expert press 'e'. \nTo quit press 'q'.")
-        except ValueError:
-            print("""Please enter a valid option.
-            For Beginner press 'b', for Intermediate press 'i', for Expert press 'e'.
-            To quit press 'q'.\n
-            """)
-            continue
-        if user_input == 'b':
-            grid = 5
-            print(f'You entered: {user_input}. You chose beginner.\n')
-        elif user_input == 'i':
-            grid = 7
-            print(f'You entered: {user_input}. You chose intermediate.\n')
-        elif user_input == 'e':
-            grid = 10
-            print(f'You entered: {user_input}. You chose expert.\n')
-        else:
-            print(f'You entered: {user_input}. Thanks for playing.\n')
+    print("Hello! This is minesweeper.")
+    user_input = validate_input(
+        message="For Beginner press 'b', for Intermediate press 'i', for Expert press 'e'. \nTo quit press 'q'.\n",
+        valid_inputs=('b', 'i', 'e', 'q')
+    )
+    if user_input == 'b':
+        print(f'You entered: {user_input}. You chose beginner.\n')
+        return {'grid':5,'num_mines':24}
+
+    elif user_input == 'i':
+        print(f'You entered: {user_input}. You chose intermediate.\n')
+        return {'grid':7,'num_mines':7}
+
+    elif user_input == 'e':
+        print(f'You entered: {user_input}. You chose expert.\n')
+        return {'grid':10,'num_mines':10}
+    else:
+        print(f'You entered: {user_input}. Thanks for playing.\n')
 
 
 def validate_input(message, valid_inputs):
-    # recursive function
     user_input = input(message)
     if user_input in valid_inputs:
         return user_input
@@ -56,8 +54,6 @@ def play(board, mines_coords, grid, lives, num_mines):
     exit = False
     while lives > 0 and len(dug)< num_cells_win:
         try:
-            # print(len(dug), num_mines, lives, num_cells_win)
-            # print(mines_coords)
             render_board(board)
             user_input = input(f"Choose a place to dig 0-{grid-1} , ex. row,col - 2,3.\nType quit if you wanna exit the game.\n")
             if user_input == 'quit':
@@ -70,32 +66,38 @@ def play(board, mines_coords, grid, lives, num_mines):
             if col_int >= grid or row_int >= grid:
                 raise ValueError
         except ValueError:
-            print("Invalid input")
+            print("Invalid input.")
             continue
         if [row_int, col_int] in dug or [row_int, col_int] in dug_mine:
-            print("Use new coordinates")
+            print("Use new coordinates.")
             continue
         elif [row_int, col_int] in mines_coords:
             lives -= 1
             print("Stepped on a mine.")
             print(f"Lives left {lives}")
-            board[row_int][col_int] = '*'
-            dug_mine.append([row_int, col_int])
-            choice = validate_input('Do you want to continue? Press "y" to continue and "no: to exit the game.\n', ('y', 'n'))
-            if choice == 'n':
-                exit = True
-                break
+            dig(board, row_int, col_int, dug_mine, '*')
+            if lives > 0:
+                choice = validate_input('Do you want to continue? Press "y" to continue and "no: to exit the game.\n', ('y', 'n'))
+                if choice == 'n':
+                    exit = True
+                    break
         elif [row_int, col_int] not in mines_coords:
-            board[row_int][col_int] = '-'
-            dug.append([row_int, col_int])
+            dig(board, row_int, col_int, dug, '-')
+
     if exit is True:
         return
+
     render_board(board)
-    win_lose(num_cells_dug=len(dug), num_cells_win=num_cells_win)
+    win_lose(lives_left=lives > 0)
 
 
-def win_lose(num_cells_dug, num_cells_win):
-    if num_cells_dug >= num_cells_win:
+def dig(board, row_coord, col_coord, digging_list, mark):
+    board[row_coord][col_coord] = mark
+    digging_list.append([row_coord, col_coord])
+
+
+def win_lose(lives_left):
+    if lives_left:
         print("Congrats. It's a win.")
     else:
         print("No more lives left. Game over.")
@@ -119,6 +121,8 @@ def render_board(board):
     formatted_nums_top = f"   {nums_top}"
     join_rows.insert(0, formatted_nums_top)
     join_rows.insert(1, border_str)
+    print('\n'.join(join_rows))
+
 
 
 def main():
@@ -127,9 +131,13 @@ def main():
     if grid_num_mines is None:
         return
     board, mines_coord = create_board(grid_num_mines['grid'], grid_num_mines['num_mines'])
-    # print(*board, sep="\n") # each list on new line 
-    # print(mines_coord)
     play(board, mines_coord, grid_num_mines['grid'], lives, grid_num_mines['num_mines'])
+    user_input = validate_input(
+        message="Do you want to play again?\nPress 'y' to play again, or 'n' to exit the game.\n", 
+        valid_inputs=('y','n')
+    )
+    if user_input == 'y':
+        main()
 
 
 if __name__ == '__main__':

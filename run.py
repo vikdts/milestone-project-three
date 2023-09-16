eb86cd9787df6358559d94ac979bdb9864e9e479
vikdts <>
2023-09-16 00:47:28 +0000

    Update missing comments

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import random
 
 
 def create_board(grid=5, num_mines=5):
+    """
+    Takes the arguments to create the functional board with different
+    grid size, dependant on the difficulty level of the game.
+    """
     cell = 0
     board = [[cell for c in range(grid)]for r in range(grid)]
     mines_coord = []
@@ -18,12 +22,19 @@ def create_board(grid=5, num_mines=5):
 
 
 def say_hello():
+    """
+    Welcomes the user to anticipate in the game
+    and check out the rules, then prompts the user
+    to choose a difficulty level and returns a dictionary
+    with the values for the size of the board grid and the
+    number of mines or option to exit the game.
+    """
     print("""\nHello! This is minesweeper.
 Make sure to check out the rules below.
 The board is divided into cells, with randomly distributed mines.
 Choose the difficulty level, which is based on the grid size.
 The number of mines is equal to the size of the grid.
-Each level is given three lives,so if stepping on a mine, can to continue.
+Each level is given three lifes,so if stepping on a mine, can to continue.
 The end goal is to clear the board without detonating any mines.
     """)
     user_input = validate_input(
@@ -33,7 +44,6 @@ For Expert press 'e' grid is 10x10 with 10 mines.
 To exit the game type 'quit'.\n\nUser input:""",
         valid_inputs=('b', 'i', 'e', 'quit')
     )
-    # print('user input', user_input)
     if user_input == 'b':
         print('You chose beginner level.\n')
         return {'grid': 5, 'num_mines': 5}
@@ -50,6 +60,9 @@ To exit the game type 'quit'.\n\nUser input:""",
 
 
 def validate_input(message, valid_inputs):
+    """
+    Recursor that validates input.
+    """
     user_input = input(message).lower()
     if user_input in valid_inputs:
         return user_input
@@ -57,12 +70,16 @@ def validate_input(message, valid_inputs):
         return validate_input(message, valid_inputs)
 
 
-def play(board, mines_coords, grid, lives, num_mines):
+def play(board, mines_coords, grid, lifes, num_mines):
+    """
+    Runs the main game loop, where the user tries to guess
+    a mine free cell.
+    """
     dug = []
     dug_mine = []
     num_cells_win = (grid**2) - num_mines
     exit = False
-    while lives > 0 and len(dug) < num_cells_win:
+    while lifes > 0 and len(dug) < num_cells_win:
         try:
             render_board(board)
             user_input = input(f"""\nWhere would you like to dig?
@@ -85,11 +102,11 @@ User input:""")
             print("Use new coordinates.")
             continue
         elif [row_int, col_int] in mines_coords:
-            lives -= 1
+            lifes -= 1
             print("Stepped on a mine.\n")
-            print(f"You have {lives} lives left.")
+            print(f"You have {lifes} lifes left.")
             dig(board, row_int, col_int, dug_mine, '*')
-            if lives > 0:
+            if lifes > 0:
                 choice = validate_input("""Do you want to continue?\n
 Press 'y' to continue or
 'n' to quit.
@@ -105,22 +122,31 @@ User input:""",
         return
 
     render_board(board)
-    win_lose(lives_left=lives > 0)
+    win_lose(lifes_left=lifes > 0)
 
 
 def dig(board, row_coord, col_coord, digging_list, mark):
+    """
+    Helper function
+    """
     board[row_coord][col_coord] = mark
     digging_list.append([row_coord, col_coord])
 
 
-def win_lose(lives_left):
-    if lives_left:
+def win_lose(lifes_left):
+    """
+    Defines the two outcomes of the game.
+    """
+    if lifes_left:
         print("Congratulations. It's a win!")
     else:
-        print("No more lives left. Game over.")
+        print("No more lifes left. Game over.")
 
 
 def render_board(board):
+    """
+    Renders the board, to display better experience for the user
+    """
     join_rows = []
     nums_top = ' '.join([str(n) for n in range(0, len(board[0]))])
 
@@ -144,14 +170,31 @@ def render_board(board):
 
 
 def main():
-    lives = 3
+    """
+    This function runs the main game loop for Minesweeper.
+    First renders the welcome meassage with the rules of the game,
+    prompts the player to enter an input of level difficulty, after
+    displaying the availble options and the corresponding input to
+    initialize the game or exit, if user not engaged.
+    The game consists of the player trying to guess the
+    coordinates of a cell, without hitting randomly allocated mine.
+    If the player guesses a mine-free cell correctly, it is revealed
+    on the rendered board with a dash. If guess coordinates step on a
+    mine player loses a life, and have to choose if to continue.
+    The player wins the game if guesses all the mine free cells,
+    before losing all its lifes.
+    """
+    lifes = 3
     grid_num_mines = say_hello()
+    # Returns none to exit the game
     if grid_num_mines is None:
         return
+    # takes arguments to create the board size chosen by the user
     board, mines_coord = create_board(grid_num_mines['grid'],
                                       grid_num_mines['num_mines'])
-    play(board, mines_coord, grid_num_mines['grid'], lives,
+    play(board, mines_coord, grid_num_mines['grid'], lifes,
          grid_num_mines['num_mines'])
+    # Ask if player wants to play again
     user_input = validate_input(
         message="""\nDo you want to play again?
 Press 'y' to continue or
